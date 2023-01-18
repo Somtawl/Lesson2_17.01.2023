4484e5d3ff6d1168f021b3c42a56fa3aef9b51c6
Somtawl <somtawl@yandex.ru>
2023-01-18 22:46:46 +0300

    final

---
+++ task10/task10.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int num = int.Parse(Console.ReadLine());
+string num1 = Convert.ToString(num);
+if (num1.Length != 3)
+{
+    Console.WriteLine("Неверное количество символов");
+}
+else
+{
+    Console.WriteLine("Вторая цифра {0}",num1[1]);
+}
---
+++ task13/task13.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+if (num < 0 )
+{
+    num = -num;
+}
+Console.WriteLine(num);
+string num1 = Convert.ToString(num);
+if (num1.Length < 3)
+{
+    Console.WriteLine("Число не имеет третьего символа");
+}
+else
+{
+        Console.WriteLine("Третий символ {0}",num1[2]);
+}
