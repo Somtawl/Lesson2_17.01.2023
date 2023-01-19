6e1a4552f1e301df58a3fc4459efa3d5c6add11e
Somtawl <somtawl@yandex.ru>
2023-01-19 18:58:01 +0300

    Сделли задачу 13 v2

---
+++ task13_v2/task13_v2.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num > 99)
+{
+    int numbers = (int)Math.Log10(num) - 2;
+    int ZeroCount = 0;
+    int divisor  = 1;
+    while (ZeroCount < numbers)
+    {
+        divisor = divisor * 10;
+        ZeroCount++;
+    }
+    int result = num / divisor % 10;
+    Console.WriteLine("Третий символ число {0} равен {1}",num,result);
+}
+else
+{
+    Console.WriteLine("В числе {0} менее 3х символов",num);
+}
